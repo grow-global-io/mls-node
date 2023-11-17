3c62763634d30222ba1ae526d0c6671245adedde
Somasekhar Praneeth <somasekhar.puralasetti@ateliasoftware.com>
2023-11-17 10:25:01 +0530

    Refactor routes and remove unused dependencies

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require("express")();
 const {getUserInfoMiddleware,verifyToken} = require("./Middleware");
 const {successResponse, errorResponse} = require("./responses");
+const authRoute = require("./routes/auth");
+const agentRoute = require("./routes/agent");
+const developerRoute = require("./routes/developer");
+const imageRoute = require("./routes/image");
+const listingRoute = require("./routes/listings");
+const propertyRoute = require("./routes/properties");
 
 app.get('/callback', (req, res) => {
   res.send("hello")
@@ -12,5 +18,11 @@ app.get('/verify',verifyToken ,(req, res) => {
 app.get('/userDetails',getUserInfoMiddleware ,(req, res) => {
   successResponse(res, req.user, "success")
 });
+app.use('/auth',authRoute);
+app.use('/agent',agentRoute);
+app.use('/developer',developerRoute);
+app.use('/image',imageRoute);
+app.use('/listing',listingRoute);
+app.use('/property',propertyRoute);
 
 app.listen(8000, () => console.log('Server listening on port 8000!'));
