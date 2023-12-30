0e74be653317f867479d022c849855f6ceb7cb56
somashekar praneeth <praneeth4138@gmail.com>
2023-12-30 16:02:23 +0530

    matches updated with location

--- constants/main.js
+++ constants/main.js
@@ -1,17 +1,24 @@
 function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the Earth in kilometers
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const earthRadius = 6371; // Earth's radius in kilometers
+
+    const toRadians = degrees => {
+        return degrees * (Math.PI / 180);
+    };
+
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+
     const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * (Math.PI / 180)) *
-        Math.cos(lat2 * (Math.PI / 180)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
+        Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
+
+    const distance = earthRadius * c; // Distance in kilometers
     return distance;
 }
+
 const calculateSimilarityScore = (data, filters) => {
     let score = 0;
 
